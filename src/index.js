3c44cdb64aebe5a1c97a73272942dc66d10436bc
Artyom Andreev <andreev.artiom@gmail.com>
2021-08-21 01:22:55 +0300

    init: start task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let number = Math.abs(n);
+
+    let digits = [];
+    while (number > 0) {
+        digits.push(number % 10);
+        number = parseInt(number / 10);
+    }
+
+    return +digits.join('');
 }
